1b9495a5789c2a9d90b5e02ad42058fd253494c1
Luboš Stránský <lubosstransky2008@gmail.com>
2026-04-14 12:20:16 +0200

    Add files via upload

---
+++ Code/LF-MK1.0_Ultrasonic_Test/LF-MK1.0_Ultrasonic_Test.ino
@@ -0,0 +1,31 @@
+#define MAX_RANG 520         //maximální dosah
+#define ADC_SOLUTION 1023.0  //ADC přesnost pro Arduino Nano
+
+bool isOn = false;
+int sensityPin = A0;
+void setup() {
+  // Serial init
+  Serial.begin(19200);
+}
+float dist_t, sensity_t;
+void loop() {
+  sensity_t = analogRead(sensityPin);
+
+  dist_t = sensity_t * MAX_RANG / ADC_SOLUTION;
+  Serial.print(dist_t, 0);
+  Serial.println("cm");
+
+  if (dist_t < 30){
+    isOn = !isOn;
+  }
+  
+  if (isOn) {
+    //zapnuté
+  } else {
+    //vypnuté
+  }
+
+  delay(500);
+}
+
+//https://wiki.dfrobot.com/sen0307/docs/18358
